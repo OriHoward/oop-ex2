66ff84862f23625c2b8bc34b5c814e816673a285
OriHoward <ori.howard@gmail.com>
2021-11-29 19:57:12 +0200

    small steps

--- src/main/java/GraphNode.java
+++ src/main/java/GraphNode.java
@@ -1,18 +1,39 @@
 import api.GeoLocation;
 import api.NodeData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GraphNode implements NodeData {
     private GeoLocation location;
     private int id;
     private double weight;
-
+    private HashMap<Integer, ArrayList<GraphEdge>> destMap;
+    private HashMap<Integer, ArrayList<GraphEdge>> sourceMap;
 
     public GraphNode(GeoLocation location, int id, double weight) {
         this.location = new NodeLocation(location.x(), location.y(), location.z());
         this.id = id;
         this.weight = weight;
+        this.destMap = new HashMap<>();
+        this.sourceMap = new HashMap<>();
+
+    }
+
+    public void addDest(GraphEdge edge) {
+        destMap.get(edge.getDest()).add(edge);
+    }
+
+    public void addSrc(GraphEdge edge) {
+        sourceMap.get(edge.getSrc()).add(edge);
+    }
+
+    public void removeDest(GraphEdge edge) {
+        destMap.get(edge.getSrc()).remove(edge);
+    }
+
+    public void removeSrc(GraphEdge edge) {
+        sourceMap.get(edge.getSrc()).remove(edge);
     }
 
     @Override
