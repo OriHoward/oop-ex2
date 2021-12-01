1b5371146fd56169c287a0496de5b88521d20683
EitanKats <eitan.kats96@gmail.com>
2021-12-01 19:29:34 +0200

    added Dijkstra implementation

--- src/main/java/DirectedGraphAlgorithms.java
+++ src/main/java/DirectedGraphAlgorithms.java
@@ -1,9 +1,9 @@
 import api.DirectedWeightedGraph;
 import api.DirectedWeightedGraphAlgorithms;
+import api.EdgeData;
 import api.NodeData;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class DirectedGraphAlgorithms implements DirectedWeightedGraphAlgorithms {
 
@@ -41,6 +41,41 @@ public class DirectedGraphAlgorithms implements DirectedWeightedGraphAlgorithms 
 
     @Override
     public List<NodeData> shortestPath(int src, int dest) {
+        Double[] dist = new Double[this.currGraph.nodeSize()];
+        dist[src] = 0.0;
+        Comparator<NodeData> byWeight = Comparator.comparing((NodeData n) -> dist[n.getKey()]);
+
+        Queue<NodeData> toScan = new PriorityQueue<>(byWeight.reversed());
+        Iterator<NodeData> nodesIter = this.currGraph.nodeIter();
+
+        ArrayList<NodeData>[] prev = new ArrayList[this.currGraph.nodeSize()];
+        while (nodesIter.hasNext()) {
+            NodeData currNode = nodesIter.next();
+            if (currNode.getKey() != src) {
+                dist[currNode.getKey()] = Double.valueOf(Integer.MAX_VALUE);
+                prev[currNode.getKey()] = new ArrayList<>();
+            }
+            toScan.add(currNode);
+        }
+
+        while (!toScan.isEmpty()) {
+            NodeData currNode = toScan.remove();
+            Iterator<EdgeData> edgeIter = this.currGraph.edgeIter(currNode.getKey());
+            EdgeData currEdge;
+            while (edgeIter.hasNext()) {
+                currEdge = edgeIter.next();
+                NodeData neighbor = this.currGraph.getNode(currEdge.getDest());
+                double alt = dist[currNode.getKey()] + currEdge.getWeight();
+                if (alt < dist[neighbor.getKey()]) {
+                    dist[neighbor.getKey()] = alt;
+                    prev[neighbor.getKey()].add(currNode);
+                    toScan.add(neighbor);
+                }
+            }
+        }
+
+        //at this point in the code I know what is the distance between the source node and every other node in the graph
+        //I need to sort the nodes by the distance so that I can start iterating over them
         return null;
     }
 
