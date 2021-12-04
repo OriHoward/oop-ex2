35869fc385ac6b2338ba0dfad711e759899491a9
EitanKats <eitan.kats96@gmail.com>
2021-12-04 20:28:45 +0200

    added tests to each iterator that throws exceptions

--- src/test/java/DirectedGraphTest.java
+++ src/test/java/DirectedGraphTest.java
@@ -1,21 +1,75 @@
+import api.DirectedWeightedGraph;
+import api.DirectedWeightedGraphAlgorithms;
+import api.EdgeData;
+import api.NodeData;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 class DirectedGraphTest {
 
+    static DirectedWeightedGraphAlgorithms algos;
+    static int counter = 0;
+
+
+    @BeforeAll
+    static void setUp() {
+        algos = new DirectedGraphAlgorithms();
+        algos.load("dataTests/isConnected.json");
+    }
+
     @BeforeEach
-    void setUp() {
+    void resetVars() {
+        counter = 0;
     }
 
     @Test
     void nodeIter() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            DirectedWeightedGraph graph = algos.getGraph();
+            Iterator<NodeData> currIter = graph.nodeIter();
+            while (currIter.hasNext()) {
+                NodeData edy = currIter.next();
+                if (counter == 1) {
+                    graph.connect(0, 4, 1.6);
+                }
+                counter += 1;
+            }
+        });
     }
 
     @Test
     void edgeIter() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            DirectedWeightedGraph graph = algos.getGraph();
+            Iterator<EdgeData> currIter = graph.edgeIter();
+            while (currIter.hasNext()) {
+                EdgeData edy = currIter.next();
+                if (counter == 1) {
+                    graph.connect(0, 4, 1.6);
+                }
+                counter += 1;
+            }
+        });
     }
 
     @Test
     void testEdgeIter() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            DirectedWeightedGraph graph = algos.getGraph();
+            Iterator<EdgeData> currIter = graph.edgeIter(0);
+            while (currIter.hasNext()) {
+                EdgeData edy = currIter.next();
+                if (counter == 1) {
+                    graph.connect(0, 4, 1.6);
+                }
+                counter += 1;
+            }
+        });
     }
 }
