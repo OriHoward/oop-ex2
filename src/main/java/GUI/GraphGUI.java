1685a443bcd9636c27ae51d90b4a73a4921dab04
OriHoward <ori.howard@gmail.com>
2021-12-07 21:15:10 +0200

    Gui progress

--- src/main/java/GUI/GraphGUI.java
+++ src/main/java/GUI/GraphGUI.java
@@ -2,18 +2,23 @@ package GUI;
 
 import RunAlgos.Ex2;
 import algos.DirectedGraphAlgorithms;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import algos.GraphNode;
+import api.NodeData;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Side;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
@@ -22,6 +27,8 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static javafx.scene.paint.Color.GREEN;
 
@@ -37,29 +44,68 @@ public class GraphGUI extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Graph");
         root = new Pane();
-        Button exitBtn = new Button("exit");
-        exitBtn.setOnAction(e -> Platform.exit());
-        exitBtn.setLayoutX(WIDTH - 50);
-        exitBtn.setLayoutY(0);
-        root.getChildren().add(exitBtn);
+
 
         final FileChooser fileChooser = new FileChooser();
-        MenuButton menuButton = new MenuButton("Options");
-        MenuItem firstItem = new MenuItem("reload different graph");
-        MenuItem secondItem = new MenuItem("clean");
-        firstItem.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                File file = fileChooser.showOpenDialog(primaryStage);
-                if (file != null) {
-                    System.out.println(file.getAbsoluteFile());
-                    Ex2.runGUI(file.getAbsolutePath());
+        MenuButton file = new MenuButton("File");
 
-                }
+        MenuItem load = new MenuItem("Load");
+        load.setOnAction(actionEvent -> {
+            File file1 = fileChooser.showOpenDialog(primaryStage);
+            if (file1 != null) {
+                Ex2.runGUI("data/G3.json");
             }
         });
+        MenuItem save = new MenuItem("Save");
 
-        menuButton.getItems().addAll(firstItem, secondItem);
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(e -> Platform.exit());
+
+        MenuButton run = new MenuButton("Run");
+        MenuItem shortestPath = new MenuItem("shortest path");
+        run.setLayoutX(50);
+        shortestPath.setOnAction(actionEvent -> {
+            Stage stage = new Stage();
+            stage.setTitle("enter source and dest");
+            TextField textField = new TextField("enter source");
+            StackPane stackPane = new StackPane();
+            stackPane.getChildren().addAll(textField);
+            Scene scene = new Scene(stackPane, 300, 300);
+            stage.setScene(scene);
+            stage.show();
+            textField.setOnAction(e -> {
+                textField.setOnKeyPressed(keyEvent -> {
+                    if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+                        String source = textField.getText();
+                        textField.clear();
+                        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+                            String dest = textField.getText();
+                            textField.setDisable(true);
+                            stage.hide();
+                            try {
+                                List<NodeData> nodeToPass = new ArrayList<>();
+                                nodeToPass = algos.shortestPath(Integer.parseInt(source), Integer.parseInt(dest));
+
+
+                            } catch (Exception exepction) {
+                                System.out.println("did not enter numbers");
+                            }
+                        }
+
+
+                    }
+
+                });
+
+
+            });
+
+
+        });
+
+
+        run.getItems().addAll(shortestPath);
+        file.getItems().addAll(load, save, exit);
         Text text = new Text(10, 10, "click on each node to find more info");
         text.setFont(new Font(40));
         text.setFill(GREEN);
@@ -68,7 +114,7 @@ public class GraphGUI extends Application {
         text.setX(50);
         text.setVisible(true);
 
-        Group group = new Group(root, text, menuButton);
+        Group group = new Group(root, text, file, run);
 
         AnimationTimer tm = new MyTimer(algos, root, radius, nodeList, WIDTH, HEIGHT);
         tm.start();
