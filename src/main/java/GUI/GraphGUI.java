f85e7cda033069b493add38d1ee703dc66554946
OriHoward <ori.howard@gmail.com>
2021-12-08 12:54:25 +0200

    Gui progress - added algorithms options and actions

--- src/main/java/GUI/GraphGUI.java
+++ src/main/java/GUI/GraphGUI.java
@@ -4,13 +4,11 @@ import algos.DirectedGraphAlgorithms;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -19,6 +17,7 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -38,18 +37,23 @@ public class GraphGUI extends Application {
         primaryStage.setTitle("Graph");
         root = new Pane();
         nodeList.clear();
+        FileChooser fileChooser = new FileChooser();
+
         MenuButton file = new MenuButton("File");
         MenuItem load = new MenuItem("Load");
         MenuItem save = new MenuItem("Save");
         MenuItem exit = new MenuItem("Exit");
-        exit.setOnAction(e -> Platform.exit());
-        MenuButton run = new MenuButton("Run");
-        MenuItem shortestPath = new MenuItem("shortest path");
+
+        MenuButton run = new MenuButton("RunAlgorithm");
+        MenuItem isConnected = new MenuItem("isConnected");
+        MenuItem shortestPathDist = new MenuItem("shortestPathDist");
+        MenuItem shortestPath = new MenuItem("shortestPath");
+        MenuItem center = new MenuItem("center");
+        MenuItem tsp = new MenuItem("tsp");
         run.setLayoutX(50);
 
 
-        final FileChooser fileChooser = new FileChooser();
-        run.getItems().addAll(shortestPath);
+        run.getItems().addAll(isConnected, shortestPathDist, shortestPath, center, tsp);
         file.getItems().addAll(load, save, exit);
         Text text = new Text(10, 10, "click on each node to find more info");
         text.setFont(new Font(40));
@@ -68,6 +72,7 @@ public class GraphGUI extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        // File options:
         load.setOnAction(actionEvent -> {
             File chosenFile = fileChooser.showOpenDialog(primaryStage);
             if (chosenFile != null) {
@@ -84,60 +89,89 @@ public class GraphGUI extends Application {
         });
 
         save.setOnAction(actionEvent -> {
-            if (algos.save("myGraph")) {
-                Stage popWindow=new Stage();
-                popWindow.initModality(Modality.APPLICATION_MODAL);
-                Label label= new Label("Graph saved successfully!");
-                Button button= new Button("close");
-                button.setOnAction(e -> popWindow.close());
-                VBox layout= new VBox(10);
-                layout.getChildren().addAll(label, button);
-                layout.setAlignment(Pos.CENTER);
-                Scene popScene= new Scene(layout, 150, 100);
-                popWindow.setScene(popScene);
-                popWindow.showAndWait();
-            }
+            savePopWindow(algos.save("myGraph"));
+        });
+        exit.setOnAction(e -> Platform.exit());
+
+
+        // run Algorithms options:
+        isConnected.setOnAction(actionEvent -> {
+            isConnectedPopWindow(algos.isConnected());
         });
 
-//        shortestPath.setOnAction(actionEvent -> {
-//            Stage stage = new Stage();
-//            stage.setTitle("enter source and dest");
-//            TextField textField = new TextField("enter source");
-//            StackPane stackPane = new StackPane();
-//            stackPane.getChildren().addAll(textField);
-//            Scene scene = new Scene(stackPane, 300, 300);
-//            stage.setScene(scene);
-//            stage.show();
-//            textField.setOnAction(e -> {
-//                textField.setOnKeyPressed(keyEvent -> {
-//                    if (keyEvent.getCode().equals(KeyCode.ENTER)) {
-//                        String source = textField.getText();
-//                        textField.clear();
-//                        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
-//                            String dest = textField.getText();
-//                            textField.setDisable(true);
-//                            stage.hide();
-//                            try {
-//                                List<NodeData> nodeToPass = new ArrayList<>();
-//                                nodeToPass = algos.shortestPath(Integer.parseInt(source), Integer.parseInt(dest));
-//
-//
-//                            } catch (Exception exepction) {
-//                                System.out.println("did not enter numbers");
-//                            }
-//                        }
-//
-//
-//                    }
-//
-//                });
-//
-//
-//            });
-//
-//
-//        });
 
+        shortestPath.setOnAction(actionEvent -> {
+            Stage stage = new Stage();
+            TextField userInput = new TextField("enter source and press OK ");
+            Button ok = new Button("OK");
+            ok.setOnAction(e -> isInt(userInput,userInput.getText()));
+            VBox layout = new VBox(10);
+            layout.setPadding(new Insets(20,20,20,20));
+            layout.getChildren().addAll(userInput,ok);
+            Scene sc = new Scene(layout,300,300);
+            stage.setScene(sc);
+            stage.show();
+        });
+
+
+    }
+
+    private void isInt(TextField userInput, String number) {
+        try {
+            int num = Integer.parseInt(number);
+            userInput.clear();
+            userInput.setStyle(".text-field.error {\n" +
+                    "  -fx-text-box-border: green ;\n" +
+                    "  -fx-focus-color: green ;\n" +
+                    "}");
+            System.out.println("num is:" + number);
+        }catch (NumberFormatException e) {
+            userInput.setStyle(".text-field.error {\n" +
+                    "  -fx-text-box-border: red ;\n" +
+                    "  -fx-focus-color: red ;\n" +
+                    "}");
+
+            System.out.println("ERROR");
+        }
+    }
+
+    private void savePopWindow(Boolean isSaved) {
+        Stage popWindow = new Stage();
+        popWindow.initModality(Modality.APPLICATION_MODAL);
+        Label label;
+        if (isSaved) {
+            label = new Label("Graph saved successfully!");
+        }
+        else {
+            label = new Label("could not save graph");
+        }
+        Button button = new Button("close");
+        button.setOnAction(e -> popWindow.close());
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, button);
+        layout.setAlignment(Pos.CENTER);
+        Scene popScene = new Scene(layout, 150, 100);
+        popWindow.setScene(popScene);
+        popWindow.showAndWait();
+    }
+
+    private void isConnectedPopWindow(Boolean isConnected) {
+        Stage popWindow = new Stage();
+        popWindow.initModality(Modality.APPLICATION_MODAL);
+        Label label;
+        if (isConnected) {
+            label = new Label("the graph is connected!");
+        } else {
+            label = new Label("the graph is NOT connected!");
+        }
+        Button button = new Button("close");
+        button.setOnAction(e -> popWindow.close());
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, button);
+        layout.setAlignment(Pos.CENTER);
+        Scene popScene = new Scene(layout, 250, 200);
+        popWindow.setScene(popScene);
+        popWindow.showAndWait();
     }
 
 
